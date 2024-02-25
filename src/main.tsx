e944090981f445cde575e4a10f462c48aff78c40
Adnan Sheikh <adnan.mohammad@scrapehero.com>
2024-02-25 17:53:05 +0530

    setup react query and react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,26 @@
+import { QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import ReactDOM from "react-dom/client";
+
 import { App } from "./App.tsx";
 import "./index.css";
 import "./tailwind-output.css";
+import { queryClient } from "./lib/react-query.ts";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   </React.StrictMode>
 );
