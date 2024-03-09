3e08d6acdf63f18ba6ce9bbb74f4d8b3ddfa1453
Adnan Sheikh <adnan.localhost@popos>
2024-03-09 13:13:44 +0530

    perfectly align skeleton

--- src/features/weather-dashboard/components/WeatherDisplaySkeleton.tsx
+++ src/features/weather-dashboard/components/WeatherDisplaySkeleton.tsx
@@ -1,61 +1,44 @@
 import { Skeleton } from "@/components/ui/skeleton";
-import { SVGSkeleton } from "./Skeleton";
 
 export const WeatherDisplaySkeleton = () => (
-  <>
-    <div className="grid gap-4 pt-4 p-6">
-      <div className="flex flex-col gap-1.5">
+  <div className="w-full flex-1">
+    <div className="flex flex-col gap-4 pt-4 p-6">
+      <div className="flex flex-col justify-center gap-1.5 h-[66px]">
         <h2>
-          <Skeleton className="w-[64px] max-w-full" />
+          <Skeleton className="h-[30px] w-[350px] max-w-full" />
         </h2>
         <p>
-          <Skeleton className="w-[104px] max-w-full" />
+          <Skeleton className="h-[20px] w-[100px] max-w-full" />
         </p>
       </div>
       <div className="grid gap-2 md:grid-cols-2">
         <div className="flex flex-row items-center gap-4">
-          <SVGSkeleton className="bg-gray-200 rounded-full overflow-hidden dark:bg-gray-800 w-[100px] h-[100px]" />
+          <Skeleton className="bg-gray-200 rounded-full dark:bg-gray-800 w-[80px] h-[80px]" />
           <div className="grid gap-1.5">
             <p>
-              <Skeleton className="w-[32px] max-w-full" />
+              <Skeleton className="w-[200px] h-[40px] max-w-full" />
             </p>
             <div className="flex flex-row items-center gap-4">
               <p>
-                <Skeleton className="w-[120px] max-w-full" />
+                <Skeleton className="w-[120px] h-[20px] max-w-full" />
               </p>
               <p>
-                <Skeleton className="w-[104px] max-w-full" />
+                <Skeleton className="w-[100px] h-[20px] max-w-full" />
               </p>
             </div>
           </div>
         </div>
         <div className="flex flex-col gap-2">
-          <div className="flex flex-row items-center gap-4">
-            <SVGSkeleton className="w-[24px] h-[24px]" />
-            <p>
-              <Skeleton className="w-[56px] max-w-full" />
-            </p>
-          </div>
-          <div className="flex flex-row items-center gap-4">
-            <SVGSkeleton className="w-[24px] h-[24px]" />
-            <p>
-              <Skeleton className="w-[56px] max-w-full" />
-            </p>
-          </div>
-          <div className="flex flex-row items-center gap-4">
-            <SVGSkeleton className="w-[24px] h-[24px]" />
-            <p>
-              <Skeleton className="w-[136px] max-w-full" />
-            </p>
-          </div>
-          <div className="flex flex-row items-center gap-4">
-            <SVGSkeleton className="w-[24px] h-[24px]" />
-            <p>
-              <Skeleton className="w-[88px] max-w-full" />
-            </p>
-          </div>
+          {[...Array(3)].map((_, idx) => (
+            <div key={idx} className="flex flex-row items-center gap-4">
+              <Skeleton className="w-[32px] h-[32px]" />
+              <p>
+                <Skeleton className="w-[80px] h-[32px] max-w-full" />
+              </p>
+            </div>
+          ))}
         </div>
       </div>
     </div>
-  </>
+  </div>
 );
